2c7a1ed87d39fbb3458f6495d75e4cee626dea41
Santiago Argilla <sargilla@gmail.com>
2020-02-19 17:06:32 -0300

    con busqueda lista

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php 
 
 require 'conection.php';
-// var_dump($db);
 
+function buscarEnMovies($db,$aguja){
+    $query = $db->prepare("SELECT * FROM movies WHERE title LIKE :busqueda");
+    $query->execute([
+        'busqueda' => $aguja
+    ]);
+    return  $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function buscarEnSeries($db,$aguja){
+    $query = $db->prepare("SELECT *, genres.name FROM series,genres WHERE title LIKE :busqueda AND series.genre_id = genres.id");
+    $query->execute([
+        'busqueda' => $aguja
+    ]);
+    return  $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+if(isset($_GET['buscar']) && $_GET['buscar'] != ''){
+    $busqueda = $_GET['buscar'];
+    if(isset($_GET['tipo'])){
+        if($_GET['tipo'] == 'pelicula'){
+            $resultados = buscarEnMovies($db,"%$busqueda%");
+        }
+        if($_GET['tipo'] == 'serie'){
+            $resultados = buscarEnSeries($db,"%$busqueda%");
+        }
+    } else {
+        $errores['tipo']= "Debe elegir un tipo de búsqueda"; 
+    }
+} else {
+    $errores['buscar'] = "Debe ingresar una búsqueda";
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -10,25 +40,38 @@ require 'conection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Series y Pelis</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php include 'header.php'; ?>
 </head>
 <body>
-<div class="center">
-        <ul class="menu">
-            <li><a href="/series.php">Series</a></li>
-            <li><a href="/peliculas.php">Pelis</a></li>
-            <li><a href="actores.php">Actores</a></li>
-        </ul>
+<div class="center <?php echo !isset($_GET['buscar']) ?  'medio' : ''?>">
+        <?php include 'menu.php'; ?>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get" >
             <div class="tipo">
-
+                <input type="radio" name="tipo"  id="movies" value="pelicula" checked/>
+                <label for="movies" >Películas</label>
+                <input type="radio" name="tipo" id="series" value="serie"/>
+                <label for="series">Series</label>
             </div>
             <div class="buscador">
                 <input type="text" name='buscar' required><button type="submit">Buscar</button>
             </div>
         </form>
+        <div class="resultados">
+        <?php if(isset($resultados)): ?>
+            <h3><?=count($resultados) ?> Resultados</h3>
+            <?php foreach($resultados as $resultado) :?>
+                <div class="items">
+                    <h3><a href="<?=$_GET['tipo']?>.php?id=<?=$resultado['id']?>"><?= $resultado['title'] ?></a></h3>
+                    <?php if($_GET['tipo'] == 'pelicula'): ?>
+                        <p><strong>Rating:</strong> <?= $resultado['rating'] ?> -<strong>Premios</strong> <?= $resultado['awards'] ?></p>
+                    <?php endif; ?>
+                    <?php if($_GET['tipo'] == 'serie'): ?>
+                        <p><strong>Género:</strong> <?= $resultado['name'] ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach ?>
+        <?php endif; ?>
+        </div>
     </div>
 </div>
     
